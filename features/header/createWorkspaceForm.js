94d52e33a047e7b41c5cd11fc8873dcb5aa70a60
Muhammed Ahad <maaahad@Muhammeds-MacBook-Pro.local>
2021-11-06 20:24:04 +0100

    Custom Select Option is added ... TODO: need some rafactoring

--- features/header/createWorkspaceForm.js
+++ features/header/createWorkspaceForm.js
@@ -1,15 +1,95 @@
 // react
-
+import React, { useRef, useReducer, useEffect, useState } from "react";
 // nextjs
 import Image from "next/image";
 
 // react icons
 import { BiX } from "react-icons/bi";
 
+import { VscChevronDown } from "react-icons/vsc";
 // in-house hooks
 import { useInput } from "../../lib/hooks";
 // sass styles
 import styles from "../../styles/header/createWorkspaceForm.module.sass";
+
+const typeSelectOptions = [
+  "Small Business",
+  "Sales CRM",
+  "Operations",
+  "Engineering-IT",
+  "Human Resources",
+  "Education",
+  "Marketing",
+];
+
+function SelectOptions({ style, toggleDisplayOptions = (f) => f }) {
+  return (
+    <div style={{ ...style }} className={styles.selectOptions}>
+      {typeSelectOptions.map((option, index) => (
+        <div key={index} onClick={toggleDisplayOptions}>
+          {option}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function CustomSelect() {
+  const selectRef = useRef();
+  const selectOptionsRef = useRef();
+  const [optionsComponent, setOptionsComponent] = useState(null);
+  // || todo : need useModal like hooks
+  const [displayOptions, toggleDisplayOptions] = useReducer(
+    (displayOptions) => {
+      if (displayOptions) {
+        setOptionsComponent(null);
+      } else {
+        const { bottom } = selectRef.current.getBoundingClientRect();
+        setOptionsComponent(
+          <SelectOptions
+            toggleDisplayOptions={toggleDisplayOptions}
+            style={{ top: `${bottom}px`, left: 0 }}
+          />
+        );
+      }
+      return !displayOptions;
+    },
+    false
+  );
+
+  const onLabelClick = () => {
+    selectRef.current.focus();
+  };
+
+  //   const onSelectControllerClick = (event) => {
+  //     event.stopPropagation();
+  //     console.log(selectRef.current.getBoundingClientRect());
+  //     // selectRef.current.nextElementSidbling.style.color = "red";
+  //     toggleDisplayOptions();
+  //   };
+
+  return (
+    <>
+      <label htmlFor="workspaceType" onClick={onLabelClick}>
+        Workspace type
+      </label>
+      <div
+        id="workspaceType"
+        ref={selectRef}
+        tabIndex={0}
+        onClick={toggleDisplayOptions}
+        className={styles.customSelect}
+      >
+        <div>Choose...</div>
+        <div>
+          <VscChevronDown />
+        </div>
+      </div>
+      {/* Select options */}
+      {optionsComponent}
+    </>
+  );
+}
 
 export default function CreateWorkspaceForm({ toggleModal = (f) => f }) {
   const [titleProps, ,] = useInput("");
@@ -48,14 +128,16 @@ export default function CreateWorkspaceForm({ toggleModal = (f) => f }) {
             onChange={titleProps.onChange}
             placeholder="Taco's Co."
           />
-          <span>This is the name of your company, team or organization.</span>
+          <span className={styles.additionalInputInfo}>
+            This is the name of your company, team or organization.
+          </span>
         </div>
 
         <div className={styles.workspaceType}>
-          <label htmlFor="title">Workspace type</label>
+          <CustomSelect />
           {/* need a separate component will all the possible options */}
-          <select
-            id="title"
+          {/* <select
+            id="workspaceType"
             value={workspaceTypeProps.value}
             onChange={workspaceTypeProps.onChange}
           >
@@ -63,12 +145,13 @@ export default function CreateWorkspaceForm({ toggleModal = (f) => f }) {
             <option value="small-business">Small Business</option>
             <option value="education">Educations</option>
             <option value="marketing">Marketing</option>
-          </select>
+          </select> */}
         </div>
 
         <div className={styles.workspaceDescription}>
-          <label id="workspaceDescr">
-            Workspace description <span>Optionsl</span>
+          <label htmlFor="workspaceDescr">
+            Workspace description{" "}
+            <span className={styles.optional}>Optional</span>
           </label>
           <textarea
             id="workspaceDescr"
@@ -76,7 +159,7 @@ export default function CreateWorkspaceForm({ toggleModal = (f) => f }) {
             onChange={workspaceDescriptionProps.onChange}
             placeholder="Our team organizes everything here."
           />
-          <span>
+          <span className={styles.additionalInputInfo}>
             Get your members on board with a few words about your Workspace.
           </span>
         </div>
